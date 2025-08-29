3401002d488869547df63a64bdd1ce9d8d498eaa
InfiniteVoid07 <harshitchaudhary027@gmail.com>
2025-08-29 11:51:11 +0530

    changes to host on vercel

--- index.js
+++ index.js
@@ -1,61 +1,8 @@
-import express from "express";
-
+import express from 'express';
 const app = express();
-app.use(express.json());
 
-app.post("/bfhl", (req, res) => {
-  try {
-    const data = req.body.data || [];
-
-    const user_id = "harshit_chaudhary_01072003";
-    const email = "harshit.chaudhary2022@vitstudent.ac.in";
-    const roll_number = "22BRS1277";
-
-    const even_numbers = [];
-    const odd_numbers = [];
-    const alphabets = [];
-    const special_characters = [];
-    let sum = 0;
-    let alphaConcat = "";
-
-    data.forEach((item) => {
-      if (/^-?\d+$/.test(item)) {
-        const num = parseInt(item, 10);
-        sum += num;
-        if (num % 2 === 0) even_numbers.push(item);
-        else odd_numbers.push(item);
-      } else if (/^[a-zA-Z]+$/.test(item)) {
-        alphabets.push(item.toUpperCase());
-        alphaConcat += item;
-      } else {
-        special_characters.push(item);
-      }
-    });
-
-    let concat_string = "";
-    let toggle = true;
-    for (let i = alphaConcat.length - 1; i >= 0; i--) {
-      concat_string += toggle
-        ? alphaConcat[i].toUpperCase()
-        : alphaConcat[i].toLowerCase();
-      toggle = !toggle;
-    }
-
-    res.status(200).json({
-      is_success: true,
-      user_id,
-      email,
-      roll_number,
-      odd_numbers,
-      even_numbers,
-      alphabets,
-      special_characters,
-      sum: sum.toString(),
-      concat_string,
-    });
-  } catch (e) {
-    res.status(500).json({ is_success: false, error: e.message });
-  }
+app.get('/', (req, res) => {
+  res.status(200).send("The API is running. Send a POST request to /bfhl to use it.");
 });
 
 export default app;
